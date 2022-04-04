9cbe44818687d534391a6eee1fe5aab0e865f78e
wastilla <wastilla@email.unc.edu>
2022-04-03 20:44:59 -0400

    Initial Commit

---
+++ FinalProject-Reminders-Recreation/FinalProject_Reminders_RecreationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProject_Reminders_RecreationApp.swift
+//  FinalProject-Reminders-Recreation
+//
+//  Created by will astilla on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalProject_Reminders_RecreationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
